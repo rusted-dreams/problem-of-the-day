f843bc6451e6f412ff38c6b4b4140e07807e5dde
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2025-01-12 19:17:14 +0530

    12th january 2025, 2116. Check if a Parentheses String Can Be Valid, almost ho hi gya tha khudse

--- 2025_01/12-01-2025_2116. Check if a Parentheses String Can Be Valid.cpp
+++ 2025_01/12-01-2025_2116. Check if a Parentheses String Can Be Valid.cpp
@@ -1,30 +1,57 @@
+//link - https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid/?envType=daily-question&envId=2025-01-12
+// problem - 2116. Check if a Parentheses String Can Be Valid
+
 #include <bits/stdc++.h>
 using namespace std;
 
-bool canBeValid(string s, string locked) {
-    stack<int> locked_st;
-    stack<int> free_st;
+// bool canBeValid(string s, string locked) {
+//     if (s.length() % 2 != 0) return false;
+//     stack<int> locked_st;
+//     stack<int> free_st;
+//     for (int i = 0; i < s.length(); i++) {
+//         cout << s[i] << " : " << locked[i] << endl;
+//         if (locked[i] == '0') free_st.push(i);
+//         else if (s[i] == '(') locked_st.push(i);
+//         else if (s[i] == ')') {
+//             if (!locked_st.empty()) locked_st.pop();
+//             else if (!free_st.empty()) free_st.pop();
+//             else return false;
+//         }
+//     }
+//     if (locked_st.empty()) return true;
+//     else {
+//         while (!locked_st.empty()) {
+//             if (free_st.empty()) return false;
+//             else if (locked_st.top() > free_st.top()) return false;
+//             else {
+//                 locked_st.pop();
+//                 free_st.pop();
+//             }
+//         }
+//     }
+//     return true;
+// }
 
+// optimised code with O(1) space (without stack)
+bool canBeValid(string s, string locked) {
+    if (s.length() % 2 != 0) return false;
+    int open = 0;
     for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '(') !locked[i] ? free_st.push(i) : locked_st.push(i);
-        else if (s[i] == ')' && !locked[i]) free_st.push(i);
-        else {
-            if (locked_st.empty() && free_st.empty()) return false;
-            cout << "here " << endl;
-            if (!locked_st.empty()) locked_st.pop();
-            else free_st.pop();
-        }
+        if (locked[i] == '0') open++;
+        else if (s[i] == '(') open++;
+        else if (s[i] == ')') open--;
+        if (open < 0) return false;
     }
-    cout << "here" << endl;
-    while (!locked_st.empty()) {
-        if (locked_st.top() > free_st.top()) return false;
-        locked_st.pop();
-        free_st.pop();
+
+    int close = 0;
+    for (int i = locked.length() - 1; i >= 0; i--) {
+        if (locked[i] == '0') close++;
+        else if (s[i] == ')') close++;
+        else if (s[i] == '(') close--;
+        if (close < 0) return false;
     }
-    cout << free_st.size() << endl;
-    if (free_st.size() % 2 == 0) return true;
-    else return false;
+    return true;
 }
 int main() {
-    canBeValid("))()))", "010100");
+
 }
